e970d57c2fe5ab957349991fdbe239973a8d9e58
Joe <36147110+jgendreau77@users.noreply.github.com>
2018-08-29 21:34:32 -0400

    Add files via upload

--- series/comments/index.php
+++ series/comments/index.php
@@ -1,1 +1,10 @@
+   <?php
 
+    /* Author: Joe Gendreau
+    Date: 0829/2018 */
+
+    // Uses echo language construct to outputthe text "Hello World." to the browser.
+
+     echo 'Hello World.';
+
+   ?>
