08d18d1f5ca909de5fbc0dc827b0c6dd676d5f7b
Joe <36147110+jgendreau77@users.noreply.github.com>
2018-09-24 15:34:24 -0400

    Add files via upload

---
+++ series/multi-dimensional-arrays/index.php
@@ -0,0 +1,12 @@
+<?php
+
+  $food = array('Healthy'=>
+                   array('Salad', 'Vegetable', 'Pasta'),
+                'Unhealthy'=>
+                         array('Pizza', 'Ice cream')
+                 );
+                 
+  echo $food['Unhealthy'][1];
+
+?>
+
