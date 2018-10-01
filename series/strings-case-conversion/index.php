2d51c928fb1264a5339c51dc9ac31ee0f22d15ed
Joe <36147110+jgendreau77@users.noreply.github.com>
2018-10-01 18:31:06 -0400

    Add files via upload

---
+++ series/strings-case-conversion/index.php
@@ -0,0 +1,19 @@
+<?php
+
+   if(isset($_GET['user_name'])&&!empty($_GET['user_name'])){
+   
+      $user_name = $_GET['user_name'];
+      $user_name_1c = strtolower($user_name);
+      
+      if($user_name_1c == 'alex'){
+         echo 'You are the best '.$user_name;
+       }
+
+     }
+
+?>
+
+<form action="index.php" method="GET">
+   Name: <input type="text" name="user_name"><br><br>
+   <input type="submit" value="submit">
+</form>
