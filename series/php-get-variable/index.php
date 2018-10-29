a2a95b03d7e3ba39ff5778c0eeaf649c3dbca41b
Joe <36147110+jgendreau77@users.noreply.github.com>
2018-10-29 11:41:27 -0400

    Add files via upload

---
+++ series/php-get-variable/index.php
@@ -0,0 +1,23 @@
+<?php
+
+      // 01. Explain what the following if statement is doing.
+  // It checks if the form has been submitted. //
+      if ( isset($_GET['day']) && isset($_GET['date']) && isset($_GET['year']) ) {
+        $day = $_GET['day'];
+        $date = $_GET['date'];
+        $year = $_GET['year'];
+        if ( !empty($day) && !empty($date) && !empty($year)) {
+          echo 'It is '.$day.' '.$date.' '.$year;
+        }else{
+          echo 'Fill in all fields';
+        }
+      }
+
+    ?>
+
+    <form action="index.php" method="GET">
+      Day:<br/><input type="text" name="day"><br>
+      Date:<br/><input type="text" name="date"><br>
+      Year:<br/><input type="text" name="year"><br>
+      <input type="submit" value="Submit">
+    </form>
