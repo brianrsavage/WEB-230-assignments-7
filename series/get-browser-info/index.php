ea179648ba3dbf70b34c15830fecae520f1ae61a
Joe <36147110+jgendreau77@users.noreply.github.com>
2018-10-29 17:39:13 -0400

    Add files via upload

---
+++ series/get-browser-info/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $browser = get_browser(null, true);
+
+    print_r($browser);
+
+    $browser = $browser['browser'];
+
+    echo '<h3>'. $browser . '</h3>';
+
+    if($browser != 'chrome'){
+      echo 'You\'re not using Google Chrome. You are using '.$browser.'.';
+    }
+
+ ?>
