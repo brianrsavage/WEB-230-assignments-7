a2689122c9abb83c118e29c1b36127f6e4d69a3a
Joe <36147110+jgendreau77@users.noreply.github.com>
2018-10-30 11:10:19 -0400

    Delete menu.data.php

--- series/o-c-p/assets/include/menu.data.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $menuItems = array('pages' =>
-                        array('MenuLink'=>'index.php','MenuName'=>'Home'),
-                        array('MenuLink'=>'events.php','MenuName'=>'Events'),
-                        array('MenuLink'=>'internal-communications.php','MenuName'=>'Communications')
-                   );
-
- ?>
